5f7bc2b887bf5dde525c2be06c5681fd338bfe57
uemurm <mitsuhide.uemura@gmail.com>
2021-03-17 20:41:12 +1100

    Add a method of updateScore. Update scores of player who pocketed and other players as well.

--- app/src/main/java/com/example/android/japan9_ballscorer/Frame.java
+++ app/src/main/java/com/example/android/japan9_ballscorer/Frame.java
@@ -5,7 +5,6 @@ import android.widget.TextView;
 
 public class Frame {
     private String pocketHistory;
-    private int points;
     private String pointString;
     private int rackScore;
     private int addedScore;
@@ -14,7 +13,6 @@ public class Frame {
 
     public Frame(Activity activity) {
         pocketHistory = "";
-        points = 0;
         pointString = "";
         rackScore = 0;
         addedScore = 0;
@@ -22,23 +20,35 @@ public class Frame {
         this.activity = activity;
     }
 
-    public void pocket(int ballNumber, String pocketType) {
+    public void updateScore(int n) {
+        rackScore += n;
+
+        TextView rackScoreView = (TextView) activity.findViewById(R.id.rack_score0);
+        rackScoreView.setText(String.valueOf(rackScore));
+    }
+
+    public int pocket(int ballNumber, String pocketType) {
+        int points;
+
         pocketHistory += String.valueOf(ballNumber) + pocketType;
         TextView scoreView = (TextView) activity.findViewById(R.id.pocket_history0);
         scoreView.setText(String.valueOf(pocketHistory));
 
         switch (ballNumber) {
             case 5:
-                points      += (pocketType == "S") ?  2  :  1 ;
+                points       = (pocketType == "S") ?  2  :  1 ;
                 pointString += (pocketType == "S") ? "X" : "|";
                 break;
             case 9:
-                points      += (pocketType == "S") ?   4  :  2 ;
+                points       = (pocketType == "S") ?   4  :  2 ;
                 pointString += (pocketType == "S") ? "XX" : "X";
                 break;
+            default:
+                points = 0;
         }
         TextView pointStrView = (TextView) activity.findViewById(R.id.point_string0);
         pointStrView.setText(String.valueOf(pointString));
 
+        return points;
     }
 }
--- app/src/main/java/com/example/android/japan9_ballscorer/Rack.java
+++ app/src/main/java/com/example/android/japan9_ballscorer/Rack.java
@@ -2,18 +2,30 @@ package com.example.android.japan9_ballscorer;
 
 import android.app.Activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rack {
-    private Frame frames[];
+    private         List<Frame> frames;
 
     public Rack(Activity activity) {
-        frames = new Frame[] {
-                new Frame(activity),
-                new Frame(activity),
-                new Frame(activity),
-        };
+        frames = new ArrayList<>();
+        frames.add(new Frame(activity));
+        frames.add(new Frame(activity));
+        frames.add(new Frame(activity));
     }
 
-    public void pocket(int ballNumber, String pocketType, int player) {
-        frames[player].pocket(ballNumber, pocketType);
+    public void pocket(int ballNumber, String pocketType, int playerNumber) {
+        int points = frames.get(playerNumber).pocket(ballNumber, pocketType);
+
+        int playerIndex = 0;
+        for (Frame frame: frames) {
+            if (playerIndex == playerNumber) {
+                frame.updateScore(points * (frames.size() - 1) );
+            } else {
+                frame.updateScore( -(points) );
+            }
+            playerIndex++;
+        }
     }
 }
