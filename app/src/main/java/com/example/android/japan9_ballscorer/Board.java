3421b2afca96fd4690d36d2a6ffebc9e61f05c8a
uemurm <mitsuhide.uemura@gmail.com>
2021-04-19 20:33:24 +1000

    Remove placeholder strings. Setup multiple racks.

--- app/src/main/java/com/example/android/japan9_ballscorer/Board.java
+++ app/src/main/java/com/example/android/japan9_ballscorer/Board.java
@@ -2,20 +2,31 @@ package com.example.android.japan9_ballscorer;
 
 import android.app.Activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
-    private Rack rack;
+    private List<Rack> racks;
+    private int currentRack;
     private int currentPlayer;
 
     public Board(Activity activity) {
-        rack = new Rack(activity);
+        racks = new ArrayList<>();
+        racks.add(new Rack(activity));
+        racks.add(new Rack(activity));
+
+        currentRack = 0;
         currentPlayer = 0;
     }
 
     public void nextPlayer() {
+        if (currentPlayer == 2) {
+            currentRack++;
+        }
         currentPlayer = (currentPlayer + 1) % 3;
     }
 
     public void pocket(int ballNumber, String pocketType) {
-        rack.pocket(ballNumber, pocketType, currentPlayer);
+        racks.get(currentRack).pocket(ballNumber, pocketType, currentPlayer);
     }
 }
